392483e3a2b644abafc2b9be858566a750bc43f7
David Canas <code@davidc.xyz>
2022-05-18 12:39:33 -0700

    WIP: Consolidating to a single file

--- main.tf
+++ main.tf
@@ -1,1 +1,52 @@
 # future main.tf
+terraform {
+  required_providers {
+    google = {
+        source = "hashicorp/google"
+        version = "4.21.0"
+    }
+  }
+}
+
+resource "google_cloudbuild_trigger" "bck_build_trigger" {
+  name           = "Auto-Annotator"
+  description    = "Edit links to interesting projects and docs to become annotate-able"
+  disabled       = false
+  ignored_files  = []
+  included_files = []
+
+  timeouts {}
+
+  pubsub_config {
+    topic = google_pubsub_topic.bck_gcs_pubsub.id
+  }
+
+  source_to_build {
+    uri       = "https://github.com/DXCanas/futurehack-2022-dxcanas"
+    ref       = "master"
+    repo_type = "GITHUB"
+  }
+
+  build {
+
+    options {
+      dynamic_substitutions = true
+    }
+
+    # pull response out into HTML file, save to shared dir
+    # step {
+    # }
+
+    # Take HTML from above, inject hypothes.is ahead of appropriate URLS, save to new file, will be new email body
+    # Observe article sources, to be fed to NR.
+    # step {
+    # }
+
+    # Send email body from above to end point
+    # step {
+    # }
+
+
+
+  }
+}
